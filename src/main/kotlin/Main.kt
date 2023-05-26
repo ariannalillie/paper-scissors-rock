e895785a6874c23baecfb954c30e98b9a4f7beb3
Arianna Johnson <ariannajohnson588@gmail.com>
2023-05-26 11:21:59 -0400

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val isPlaying = true
+    val score = 80
+
+    if (isPlaying || score == 100) {
+        println("One is true")
+    } else {
+        println("everything is not true")
+    }
+}
