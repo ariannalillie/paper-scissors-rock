65a7ba6fea289e4de2822e45912c574571828d91
Arianna Johnson <ariannajohnson588@gmail.com>
2023-05-28 10:05:22 -0400

    Basic rock paper scissors logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,31 @@
-fun main() {
-    val isPlaying = true
-    val score = 80
+import java.util.Random
 
-    if (isPlaying || score == 100) {
-        println("One is true")
+fun main() {
+    val userChoice = userChoice()
+    val computerChoice = computerChoice()
+    println("You chose $userChoice and the computer chose $computerChoice")
+    if (userChoice != null) {
+        determineWin(computerChoice, userChoice)
+    }
+}
+
+fun userChoice(): Int? {
+    print("Game on! Type 0 for Rock, 1 for Paper or 2 for Scissors: ")
+    val userChoice = readlnOrNull()
+    return userChoice?.toIntOrNull()
+}
+
+fun computerChoice(): Int {
+    val random = Random()
+    return random.nextInt(2)
+}
+
+fun determineWin(computerChoice: Int, userChoice: Int) {
+    if (userChoice == 0 && computerChoice == 2) {
+        println("You win!")
+    } else if (computerChoice > userChoice) {
+        println("This computer wins this round, better luck next time")
     } else {
-        println("everything is not true")
+        println("It's a tie! Play again.")
     }
 }
